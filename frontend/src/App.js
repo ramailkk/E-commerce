3f8197e06d064d0ebea3752c0fb7fb0d3a04bb97
Ramail Khan <ramail.khan@salsoft.net>
2025-08-04 17:19:16 +0500

    readying up changes for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import ProductPage from './components/ProductPage.js';
+import ProductPage from './components/ProductsPage/ProductPage.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
